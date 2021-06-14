2637fff0210ef20a6ee250ab27036ffdfbe7a62f
isfar-uddin <isfarruett@gmail.com>
2021-06-15 00:29:10 +0600

    Solve missing numbers problem of hackerrank in JS

---
+++ hackerRank_missingNumbers.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'missingNumbers' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY arr
+ *  2. INTEGER_ARRAY brr
+ */
+
+function missingNumbers(arr, brr) {
+    // Write your code here
+    let missingSet = new Set();
+    brr.filter((el) => {
+        if(arr.indexOf(el) >= 0) {
+            arr[arr.indexOf(el)] = null;
+        } else {
+            missingSet.add(el);
+        }
+    })
+    return Array.from(missingSet).sort((a,b) => a - b);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const m = parseInt(readLine().trim(), 10);
+
+    const brr = readLine().replace(/\s+$/g, '').split(' ').map(brrTemp => parseInt(brrTemp, 10));
+
+    const result = missingNumbers(arr, brr);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
