73b0ce78b08157ec69ac128ce6205013ff8b7447
isfar-uddin <isfarruett@gmail.com>
2021-07-14 15:00:45 +0600

    Add another solution for Search A 2D Matrix problem of leetcode

--- LeetCode/Search A 2D Matrix.js
+++ LeetCode/Search A 2D Matrix.js
@@ -18,4 +18,27 @@ var searchMatrix = function(matrix, target) {
     }
 
     return false;
+
+    /* Another solution which will work only if prev row last column value is less than current row first column value
+
+    if(!matrix.length) return false;
+
+    const m = matrix.length;
+    const n = matrix[0].length;
+
+    let start = 0, end = m * n - 1;
+
+    while(start <= end) {
+        const mid = Math.floor((start + end)/2);
+        let i = Math.floor(mid / n);
+        let j = mid % n;
+        if(matrix[i][j] === target) return true;
+        if(matrix[i][j] > target) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+    return false;
+    */
 };
