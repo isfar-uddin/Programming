1a9e338b223b7b1624655a34e3b8d019efdae394
isfar-uddin <isfarruett@gmail.com>
2021-07-27 01:13:16 +0600

    Add my solution after learning in Coin Change problem of leetcode

--- LeetCode/Coin Change.js
+++ LeetCode/Coin Change.js
@@ -3,7 +3,8 @@
  * @param {number} amount
  * @return {number}
  */
-var coinChange = function(coins, amount) {
+//Other's solution
+/*var coinChange = function(coins, amount) {
     let dp = Array(amount + 1).fill(amount + 1);
 
     dp[0] = 0;
@@ -16,4 +17,20 @@ var coinChange = function(coins, amount) {
         }
     }
     return dp[amount] > amount ? -1 : dp[amount];
+};
+*/
+
+//My Solution
+var coinChange = function(coins, amount) {
+    let dp = new Array(amount + 1).fill(0);
+    dp[0] = 1;
+    for(let at = 1; at <= coins.length; at++) {
+        for(let i = coins[at- 1]; i <= amount; i++) {
+            if(dp[i - coins[at- 1]]) {
+                dp[i] = dp[i] ? Math.min(dp[i], dp[i - coins[at - 1]] + 1) : dp[i - coins[at - 1]] + 1;
+            }
+        }
+    }
+
+    return dp[amount] - 1;
 };
