5ded8b5626f2f4f36f93ec61a5b2b2d4bb2328ab
isfar-uddin <isfarruett@gmail.com>
2021-08-04 03:05:19 +0600

    Add third solution of Linked List Cycle problem of leetcode

--- LeetCode/Linked List Cycle.js
+++ LeetCode/Linked List Cycle.js
@@ -50,3 +50,29 @@ var hasCycle = function(head) {
 
     return false;
 };
+
+
+// Brute force without modifying the actual linked list
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let listOfPointers = [];
+
+    while(head) {
+        if(listOfPointers.includes(head)) return true;
+        listOfPointers.push(head);
+        head = head.next;
+    }
+
+    return false;
+};
