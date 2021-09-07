da75c25df3eb9291d383fbac918489a181ae4b8e
isfar-uddin <isfarruett@gmail.com>
2021-09-08 01:50:46 +0600

    Solve Guess Number Higher or Lower problem of Leetcode

---
+++ LeetCode/Guess Number Higher Or Lower.js
@@ -0,0 +1,32 @@
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	            -1 if num is lower than the guess number
+ *			             1 if num is higher than the guess number
+ *                       otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function(n) {
+    let low = 1, high = n;
+
+    while(low < high) {
+        let mid = low + Math.floor((high - low) / 2);
+        let response = guess(mid);
+        if(response === 0) {
+            return mid;
+        }
+
+        if(response === 1) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+
+    return low;
+};
